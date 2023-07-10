cde6d571621bb0c8854c54afe9ba05732f74583e
maisam2004 <46633089+maisam2004@users.noreply.github.com>
2023-07-10 14:33:19 +0100

    Create test while loop

--- start.py
+++ start.py
@@ -10,3 +10,12 @@ whole_letters =string.ascii_letters
 print(whole_letters)
 
 print("\u2764\ufe0f")
+
+word = mirror
+clu = ['?','?','?','?','?','?']
+while True:
+    guess= input('guess you letter ')
+
+    for i,l in enumerate(word):
+        ln= word.count(l)
+        for i in range(ln):
