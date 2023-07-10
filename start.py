7b7f5db293449ccf463f68c7e5ef89721340c82b
maisam2004 <46633089+maisam2004@users.noreply.github.com>
2023-07-10 14:53:43 +0100

    Create if and elif for letter guess input

--- start.py
+++ start.py
@@ -7,15 +7,23 @@ the_word = word_generator.word()
 clue_list=list('?'*len(the_word))
 print(clue_list)
 whole_letters =string.ascii_letters
-print(whole_letters)
+#print(whole_letters)
 
-print("\u2764\ufe0f")
+heartlist=list("\u2764\ufe0f"*9)
 
-word = mirror
+word = 'mirkos'
 clu = ['?','?','?','?','?','?']
 while True:
     guess= input('guess you letter ')
-
-    for i,l in enumerate(word):
-        ln= word.count(l)
-        for i in range(ln):
+    if word.count(guess) > 0:
+        ind = word.index(guess)
+        clu[ind]=guess
+        #for i,l in enumerate(word):
+            #ln= word.count(l)
+           # for i in range(ln):
+    elif word.count(guess) == 0:
+        print('you lost one of your heart')
+        heartlist.pop()
+    if '?' not in clu:
+        print('well done',''.join(clu))
+        break
