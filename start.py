8db6da155743cd050400cc92277a2f5dbb7c6099
maisam2004 <46633089+maisam2004@users.noreply.github.com>
2023-07-10 16:28:04 +0100

    Add comments

--- start.py
+++ start.py
@@ -11,26 +11,29 @@ whole_letters =string.ascii_letters
 
 heartlist=list("\u2764"*9)
 
-word = 'mirros'
-clu = ['?','?','?','?','?','?']
-while heartlist :
-    guess= input('guess you letter ')
-    if guess not in whole_letters:
-        guess= input('PLEASE ENTER ONLY LETTERS ')
-    if guess in word and guess not in clu:
-        num_lettrs= word.count(guess)
-        for _ in range(num_lettrs):
-            ind = word.index(guess)
-            print(ind,'this index of letter')
-            clu[ind]=guess
-            word = word.replace(word[ind],'_',1)
 
-        print(''.join(clu),'<< this is your guess so far ',f'you have {len(heartlist)} lives left',sep=' ')
+
+while heartlist :
+    guess= input('guess you letter ') #request letter
+    if guess not in whole_letters:
+        guess= input('PLEASE ENTER ONLY LETTERS ') #request letter if first try did not enter nubmer
+    if guess in the_word and guess not in clue_list:
+        num_lettrs= the_word.count(guess) #find the number of times letters in word
+        new_word = the_word #copy of main word
+        for _ in range(num_lettrs): # loop to remove any doble letters
+            ind = new_word.index(guess)
+            
+            clue_list[ind]=guess
+            new_word = new_word.replace(new_word[ind],'_',1)
+
+        print(''.join(clue_list),'<< this is your guess so far ',f'you have {len(heartlist)} lives left',sep=' ')
         
     else:
         print('you lost one of your heart')
-        heartlist.pop()
+        heartlist.pop() #remove one heart
         print(heartlist,f'only this {len(heartlist)} lives left')
-    if '?' not in clu:
-        print('well done',''.join(clu))
+    if '?' not in clue_list:
+        print('well done the word was > ',''.join(clue_list),''.join(set(heartlist)))
         break
+
+print(f'the word was>>{the_word}')
