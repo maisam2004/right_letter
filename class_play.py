076d4232a5c9b14f822cf06fd821a9f7cfd12c81
maisam2004 <46633089+maisam2004@users.noreply.github.com>
2023-07-11 15:21:39 +0100

    Creat method for every part of while loop

--- class_play.py
+++ class_play.py
@@ -1,5 +1,4 @@
 import string
-import random
 from wonderwords import RandomWord
 
 class let_guess:
@@ -14,29 +13,48 @@ class let_guess:
     def play(self):
         while self.heartlist :
             guess= input('guess you letter ') #request letter
-            if guess not in self.whole_letters:
-                guess= input('PLEASE ENTER ONLY LETTERS ') #request letter if first try did not enter nubmer
-            self.all_guesses += guess
-            if guess in self.the_word and guess not in self.clue_list:
-                
-                for i,l in enumerate(self.the_word): # loop to remove any doble letters
-                    if l == guess:
-                        self.clue_list[i] = guess
 
+            guess = self.check_guess(guess)
+            self.all_guesses += guess 
+            if guess in self.the_word and guess not in self.clue_list:
+
+                self.clue_list=self.righ_loop(guess)
                 print(''.join(self.clue_list),'<< this is your guess so far ',f'you have {len(self.heartlist)} lives left',sep=' ')
-                
+
             else:
-                print('you lost one of your heart')
-                self.heartlist.pop() #remove one heart
-                print(self.heartlist,f'only this {len(self.heartlist)} lives left')
-                if len(self.heartlist) <1:
-                    print(f'the word was>>{self.the_word}')
-                    print('all of your guesses >>','-'.join(list(self.all_guesses)))
-                    
+                self.loosing()
+
             if '?' not in self.clue_list:
-                print('well done the word was > ',''.join(self.clue_list),''.join(set(self.heartlist)))
+                self.win()
                 break
 
+    def check_guess(self,guess):
+        if guess not in self.whole_letters:
+            guess= input('PLEASE ENTER ONLY LETTERS ')
+            return guess
+        return guess
+    
+    def righ_loop(self,guess):
+        for i,l in enumerate(self.the_word): # loop to remove any doble letters
+                if l == guess:
+                    self.clue_list[i] = guess
+        return self.clue_list
+    
+    def loosing(self):
+        self.heartlist.pop() #remove one heart
+        print('you lost one of your heart')
+        print(self.heartlist,f'only this {len(self.heartlist)} lives left')
+
+        if len(self.heartlist) <1: #check if lost
+            self.lost()
+    
+    def lost(self): #lost game
+        print(f'Sory you lost :< the word was>>{self.the_word}')
+        print('all of your guesses >>','-'.join(list(self.all_guesses)))
+
+    def win(self):
+        print('well done the word was > ',''.join(self.clue_list),''.join(set(self.heartlist)))
+       
 
 
 
