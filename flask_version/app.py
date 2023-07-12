2a8f71355f16f694dabe0eba57e2e8abdc37dd8a
maisam2004 <46633089+maisam2004@users.noreply.github.com>
2023-07-12 11:51:29 +0100

    Create handle entered letter

--- flask_version/app.py
+++ flask_version/app.py
@@ -1,10 +1,14 @@
 from flask import Flask,redirect,render_template,request,url_for,aport,session
-
+from class_play import let_guess
 app = Flask(__name__)
 
 @app.route('/',methods = ['get','post'])
 def home():
     if request.method != 'post':
         return render_template('home.html')
+    letter = request.form['letter']
+
     
 
+if __name__ == "__main__":
+    app.run(debug=True)
