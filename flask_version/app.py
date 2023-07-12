fc92de71acfea7adacc937a876a17ef78a92bd63
maisam2004 <46633089+maisam2004@users.noreply.github.com>
2023-07-12 15:33:16 +0100

    Add new form to html, update flask app

--- flask_version/app.py
+++ flask_version/app.py
@@ -1,23 +1,31 @@
-from flask import Flask, redirect, render_template, request
+from flask import Flask, redirect, render_template, request, session
 import string
 from wonderwords import RandomWord
 
 app = Flask(__name__)
-word_generator = RandomWord()
-the_word = word_generator.word()
+app.secret_key = 'your-secret-key'
 
-clue_list = ['?'] * len(the_word)
-whole_letters = string.ascii_letters
-heartlist = list("\u2764" * 9)
+def generate_word():
+    word_generator = RandomWord()
+    return word_generator.word()
 
+def initialize_game():
+    session['the_word'] = generate_word()
+    session['clue_list'] = ['?'] * len(session['the_word'])
+    session['heartlist'] = list("\u2764" * 9)
 
 @app.route('/', methods=['GET', 'POST'])
 def home():
-    global the_word, clue_list, heartlist
+    if 'the_word' not in session:
+        initialize_game()
+
+    the_word = session['the_word']
+    clue_list = session['clue_list']
+    heartlist = session['heartlist']
 
     if request.method == 'POST':
         guess = request.form['letter']
-        if guess not in whole_letters:
+        if guess not in string.ascii_letters:
             message = 'PLEASE ENTER ONLY LETTERS: '
         else:
             if guess in the_word and guess not in clue_list:
@@ -25,18 +33,29 @@ def home():
                     if l == guess:
                         clue_list[i] = guess
 
-                message = ' '.join(clue_list) + f'<< This is your guess so far. You have {len(heartlist)} lives left.'
-            else:
+                message = ' '.join(clue_list) + f' << This is your guess so far. You have {len(heartlist)} lives left.'
+            elif len(heartlist) > 0:
                 heartlist.pop()
-                message = f"You lost one of your lives. Only {len(heartlist)} lives left."
+                message = f"You lost one of your lives. Only {len(heartlist)} lives left. Still continuing: {' '.join(clue_list)}"
+                if len(heartlist)==0:
+                     message = f"You lost! The word was: {the_word}"
+            else:
+                message = f"You lost! The word was: {the_word}"
 
             if '?' not in clue_list:
                 message = 'Well done! The word was: ' + ' '.join(clue_list)
+
+        session['clue_list'] = clue_list
+        session['heartlist'] = heartlist
     else:
         message = ''
 
     return render_template("home.html", message=message)
 
+@app.route('/new_word', methods=['POST'])
+def new_word():
+    initialize_game()
+    return redirect('/')
 
 if __name__ == "__main__":
     app.run(debug=True)
